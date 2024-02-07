fb34f022dea87d4d1dc9d8ef2e920ad3b842826f
Replit user <>
2024-02-07 03:33:43 +0000

    Test

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 uy = input("Uy raqamingiz: ")
 kocha = input("Ko`changiz: ")
 mahalla = input("Mahallangiz: ")
+# Nimadir
 tuman = input("Tumanningiz: ")
 viloyat = input("Viloyatingiz: ")
 manzil = f"{viloyat} viloyati {tuman} tumani {mahalla} mahallasi {kocha} ko`chasi {uy} uyda"
